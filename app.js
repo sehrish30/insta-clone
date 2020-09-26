25cc6de4e9fc3b5f903d9d1c70719e4f1ecbd810
Sehrishah <sehrishwaheed98@gmail.com>
2020-09-26 10:57:11 -0700

    deploying by heroku

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use(require('./routes/user'))
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
     const path = require('path')
-    app.get("*",(req,res)=>{
-        // res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-        res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    app.get('*',(req,res)=>{
+         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        // res.sendFile(path.join(__dirname,'./client/build/index.html'))
     })
 }
 
