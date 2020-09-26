b3a8f556f961c41262335e6113455ccff29b13a4
Sehrishah <sehrishwaheed98@gmail.com>
2020-09-26 05:40:51 -0700

    fix

---
+++ client/build/precache-manifest.f06cafd84d795d504b8d29a7e0f01ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b273717f2620fa8e1d79e2980a2abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd172d76d5b6a30d899",
+    "url": "/static/css/main.16e63311.chunk.css"
+  },
+  {
+    "revision": "6fe74a1d52c6e391bb48",
+    "url": "/static/js/2.a20a0620.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.a20a0620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd172d76d5b6a30d899",
+    "url": "/static/js/main.a87c08d9.chunk.js"
+  },
+  {
+    "revision": "12ffa9c2a2e2bca05bc1",
+    "url": "/static/js/runtime-main.a41b20c0.js"
+  }
+]);
