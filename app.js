f4809c97844e468e288baaeb492f6359d8f776aa
Sehrishah <sehrishwaheed98@gmail.com>
2020-09-26 05:53:04 -0700

    fixing unknown bugs

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(require('./routes/post'));
 app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    // app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
